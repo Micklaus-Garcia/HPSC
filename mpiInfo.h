e609a360cb4dfa01b2cac757f1e0111c8677983e
Leila Ghaffari <Leila.Ghaffari@colorado.edu>
2020-09-15 11:53:34 -0600

    Some clean-up and debugged the code to plot what is expected

--- mpiInfo.h
+++ mpiInfo.h
@@ -1,13 +1,10 @@
-
 //  ==
 //  ||
 //  ||   C L A S S:    m p i I n f o
 //  ||
 //  ==
-
-class mpiInfo
-{
- public:
+class mpiInfo {
+  public:
 
   int myPE;
   int numPE;
@@ -17,7 +14,6 @@ class mpiInfo
   int iPE , jPE;
   int iMin, iMax, jMin, jMax ; // The global i-j numbers on this processor
   int nei_n, nei_s, nei_e, nei_w;
-  int nei_ne, nei_nw, nei_se, nei_sw;   // <------ NEW
   
   int countx, county;
 
@@ -28,8 +24,8 @@ class mpiInfo
   double *phiSend_e,  *phiSend_w;
   double *phiRecv_n,  *phiRecv_s, *phiRecv_e,  *phiRecv_w;
   
-  MPI_Status status;
-  int        err;
+  MPI_Status  status;
+  int         err;
   int         tag;
   MPI_Request request;
 
@@ -62,59 +58,33 @@ class mpiInfo
   //  |
   //  |
   //  -
-  
-
-  void GridDecomposition(int _nPEx, int _nPEy, int nCellx , int nCelly)
-  {
+  void GridDecomposition(int _nPEx, int _nPEy, int nCellx, int nCelly) {
 
     nRealx = nCellx + 1;
     nRealy = nCelly + 1;
 
-    // Store and check incoming processor counts
-    
+    // Store and check incoming processor counts 
     nPEx = _nPEx;
     nPEy = _nPEy;
     
-    if (nPEx*nPEy != numPE)
-      {
-    	if ( myPE == 0 ) cout << "Fatal Error:  Number of PEs in x-y directions do not add up to numPE" << endl;
+    if (nPEx*nPEy != numPE) {
+      if ( myPE == 0 ) cout << "Fatal Error:  Number of PEs in x-y directions do not add up to numPE" << endl;
     	MPI_Barrier(MPI_COMM_WORLD);
     	MPI_Finalize();
     	exit(0);
-      }
+    }
     
-    // Get the i-j location of this processor, given its number.  See figure above:
-    
+    // Get the i-j location of this processor, given its number.  See figure above: 
     jPE = int(myPE/nPEx);
     iPE = myPE - jPE*nPEx;
 
     // Set neighbor values
-
     nei_n = nei_s = nei_e = nei_w = -1;
 
-    if ( iPE > 0      )
-      {
-	nei_w = myPE - 1    ;
-      }
-    if ( jPE > 0      )
-      {
-	nei_s = myPE - nPEx ;  
-      }
-    if ( iPE < nPEx-1 )
-      {
-	nei_e = myPE + 1    ; 
-      }
-    if ( jPE < nPEy-1 )
-      {
-	nei_n = myPE + nPEx ;  
-      }
-
-    nei_nw = nei_sw = nei_ne = nei_se = -1;
-    
-    if ( iPE > 0      && jPE > 0      )  nei_sw = myPE - 1 - nPEx  ;
-    if ( iPE < nPEx-1 && jPE > 0      )  nei_se = myPE + 1 - nPEx  ;
-    if ( iPE > 0      && jPE < nPEy-1 )  nei_nw = myPE - 1 + nPEx  ;
-    if ( iPE > nPEx-1 && jPE < nPEy-1 )  nei_ne = myPE + 1 + nPEx  ;
+    if ( iPE > 0      ) { nei_w = myPE - 1    ; }
+    if ( jPE > 0      ) { nei_s = myPE - nPEx ; }
+    if ( iPE < nPEx-1 ) { nei_e = myPE + 1    ; }
+    if ( jPE < nPEy-1 ) { nei_n = myPE + nPEx ; }
 
     // Acquire memory for the communication between adjacent processors:
     countx = nRealx + 2;
@@ -136,60 +106,56 @@ class mpiInfo
 
     tag = 0;
   }
-void PEsum(VD &field)
-         {sLOOP phiSend_n[s] = 0.;
-	  sLOOP phiSend_s[s] = 0.;
-	  tLOOP phiSend_e[t] = 0.;
-	  tLOOP phiSend_w[t] = 0.;
+  void PEsum(VD &field) {
+
+      sLOOP phiSend_n[s] = 0.;
+	sLOOP phiSend_s[s] = 0.;
+	tLOOP phiSend_e[t] = 0.;
+	tLOOP phiSend_w[t] = 0.;
  
-          tLOOP phiL[t] = field[pid(1     , t     )];
-          tLOOP phiR[t] = field[pid(nRealx, t     )];
-          sLOOP phiT[s] = field[pid(s     , nRealy)];
-          sLOOP phiB[s] = field[pid(s     , 1     )];
+      tLOOP phiL[t] = field[pid(1     , t     )];
+      tLOOP phiR[t] = field[pid(nRealx, t     )];
+      sLOOP phiT[s] = field[pid(s     , nRealy)];
+      sLOOP phiB[s] = field[pid(s     , 1     )];
 
-          sLOOP phiSend_n[s] = phiT[s];
-          sLOOP phiSend_s[s] = phiB[s];
-          tLOOP phiSend_e[t] = phiR[t];
-          tLOOP phiSend_w[t] = phiL[t];
+      sLOOP phiSend_n[s] = phiT[s];
+      sLOOP phiSend_s[s] = phiB[s];
+      tLOOP phiSend_e[t] = phiR[t];
+      tLOOP phiSend_w[t] = phiL[t];
        
-          if (nei_n>=0) err = MPI_Isend(phiSend_n, countx, MPI_DOUBLE, nei_n, tag, MPI_COMM_WORLD, &request);
-          if (nei_s>=0) err = MPI_Isend(phiSend_s, countx, MPI_DOUBLE, nei_s, tag, MPI_COMM_WORLD, &request);
-          if (nei_e>=0) err = MPI_Isend(phiSend_e, county, MPI_DOUBLE, nei_e, tag, MPI_COMM_WORLD, &request);
-          if (nei_w>=0) err = MPI_Isend(phiSend_w, county, MPI_DOUBLE, nei_w, tag, MPI_COMM_WORLD, &request);
+      if (nei_n>=0) err = MPI_Isend(phiSend_n, countx, MPI_DOUBLE, nei_n, tag, MPI_COMM_WORLD, &request);
+      if (nei_s>=0) err = MPI_Isend(phiSend_s, countx, MPI_DOUBLE, nei_s, tag, MPI_COMM_WORLD, &request);
+      if (nei_e>=0) err = MPI_Isend(phiSend_e, county, MPI_DOUBLE, nei_e, tag, MPI_COMM_WORLD, &request);
+      if (nei_w>=0) err = MPI_Isend(phiSend_w, county, MPI_DOUBLE, nei_w, tag, MPI_COMM_WORLD, &request);
 
-          if (nei_n>=0) {
+      if (nei_n>=0) {
       err = MPI_Irecv(phiRecv_n, countx, MPI_DOUBLE, nei_n, tag, MPI_COMM_WORLD, &request);   
             MPI_Wait(&request, &status); 
-    }
-          if (nei_s>=0) {
+      }
+      if (nei_s>=0) {
       err = MPI_Irecv(phiRecv_s, countx, MPI_DOUBLE, nei_s, tag, MPI_COMM_WORLD, &request);   
             MPI_Wait(&request, &status); 
-    }
-          if (nei_e>=0) {
+      }
+      if (nei_e>=0) {
       err = MPI_Irecv(phiRecv_e, county, MPI_DOUBLE, nei_e, tag, MPI_COMM_WORLD, &request);   
             MPI_Wait(&request, &status); 
-    }
-          if (nei_w>=0) {
+      }
+      if (nei_w>=0) {
       err = MPI_Irecv(phiRecv_w, county, MPI_DOUBLE, nei_w, tag, MPI_COMM_WORLD, &request);   
             MPI_Wait(&request, &status); 
-    }
+      }
 
+      if (nei_n>=0) sLOOP phiT[s] = phiRecv_n[s];
+      if (nei_s>=0) sLOOP phiB[s] = phiRecv_s[s];
+      if (nei_e>=0) tLOOP phiR[t] = phiRecv_e[t];
+      if (nei_w>=0) tLOOP phiL[t] = phiRecv_w[t];
 
-          if (nei_n>=0) sLOOP phiT[s] = phiRecv_n[s];
-          if (nei_s>=0) sLOOP phiB[s] = phiRecv_s[s];
-          if (nei_e>=0) tLOOP phiR[t] = phiRecv_e[t];
-          if (nei_w>=0) tLOOP phiL[t] = phiRecv_w[t];
-
-
-          if (nei_n>=0) sLOOP field[pid( s        , nRealy+1 )] = phiT[s];
-          if (nei_s>=0) sLOOP field[pid( s        , 0        )] = phiB[s];
-          if (nei_e>=0) tLOOP field[pid( nRealx+ 1, t        )] = phiR[t];
-          if (nei_w>=0) tLOOP field[pid( 0        , t        )] = phiL[t];
-
-
+      if (nei_n>=0) sLOOP field[pid( s        , nRealy )] += phiT[s];
+      if (nei_s>=0) sLOOP field[pid( s        , 1      )] += phiB[s];
+      if (nei_e>=0) tLOOP field[pid( nRealx   , t      )] += phiR[t];
+      if (nei_w>=0) tLOOP field[pid( 1        , t      )] += phiL[t];
    }
 
-          int pid(int i,int j) {return (i+1) + (j)*(nRealx+2);} 
-  
+   int pid(int i,int j) {return (i+1) + (j)*(nRealx+2);} 
 };
 
